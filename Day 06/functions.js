77df21e8d68a8ab847e2eb200d0f4c34f2a5bb0b
Bibhishika <83827442+Bibhishika@users.noreply.github.com>
2022-08-25 17:00:47 +0545

    Add files via upload

---
+++ Day 06/functions.js
@@ -0,0 +1,39 @@
+
+function onePlusAvg(x,y){
+    
+    return (1+(x+y)/2) 
+}
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("Average of a and b is ",onePlusAvg(a,b))
+console.log("Average of b and c is ",onePlusAvg(b,c))
+console.log("Average of a and c is ",onePlusAvg(a,c))
+
+function onePlusAvg(x,y){
+    
+    return (1+(x+y)/2) 
+}
+
+const sum = (p,q)=>{
+    return p + q
+}
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("Average of a and b is ",onePlusAvg(a,b))
+console.log("Average of b and c is ",onePlusAvg(b,c))
+console.log("Average of a and c is ",onePlusAvg(a,c))
+console.log(sum(4,8))
+
+
+const hello = ()=>{
+    console.log("Hey how are you. I am fine")
+    return "hi"
+}
+
+let v = hello();
+console.log(v)
