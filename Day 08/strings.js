404793496395d149c0d7eea3f58e636768e49ed0
Bibhishika <83827442+Bibhishika@users.noreply.github.com>
2022-08-27 22:14:22 +0545

    Add files via upload

---
+++ Day 08/strings.js
@@ -0,0 +1,19 @@
+
+
+let name = "Bibu"
+//console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let animal = 'Panda'
+console.log(animal)
+
+//template literals
+let boy1 = "John"
+let boy2 = "Arnold"
+let sentence = `${boy1} is a friend of ${boy2}` //backtick
+console.log(sentence)
+
+//escape sequence characters
+let fruit = "Gra\'pe"
+console.log(fruit)
+console.log(fruit.length)
