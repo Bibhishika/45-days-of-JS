6a6190698fc05cd40106aef9fcc9803cc29089ab
Bibhishika <83827442+Bibhishika@users.noreply.github.com>
2022-08-30 23:01:09 +0545

    Add files via upload

---
+++ Day 11/Arrays_in_js.js
@@ -0,0 +1,18 @@
+let marks = [91, 82, 63, 74, false, "Not Present"];
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+console.log(marks[4])
+console.log(marks[5])
+console.log("The length of marks",marks.length)
+marks[3]=76
+console.log(marks)
+console.log(typeof marks)
+
+*/
+let i=0;
+let marks = [12,33,21,45,43]
+for(i=0;i<marks.length;i++){
+    console.log(marks[i])
+}
